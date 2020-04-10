3c3a7dd691c8d0d6bc09f6132aca5601c0a95df9
BeaNecib <61478352+BeaNecib@users.noreply.github.com>
2020-04-10 21:13:22 +0100

    Update enigme.h

--- enigme.h
+++ enigme.h
@@ -1,5 +1,5 @@
 #ifndef  ENIGME_H_INCLUDED 
-#define ENIGME_H_INCLUDED
+#define ENIGMLE_H_INCLUDED
 
 #include<stdio.h> 
 #include<stdlib.h>
@@ -7,34 +7,24 @@
 #include<SDL/SDL_image.h> 
 #include <SDL/SDL_ttf.h>
 #include <string.h> 
-#include <SDL/SDL_mixer.h>
 
-typedef struct
+typedef struct enigmes_question
 {
-	char reponsevrai[3];
-	char quest[400];
-	char rep1[3];
-	char rep2[3];
-} enigme_question;
+char reponsevrai[3];
+char q[400];
+char rep1[3];
+char rep2[3];
+}enigmes_question;
 
-typedef struct 
+typedef struct enigme
 {
-	enigme_question EN[30];
-	int reponse;
-} enigme_reponse;
+enigmes_question EN[30];
+int reponse;
+}enigme;
 
-typedef struct 
-{
-	 SDL_Surface * image;
- 	 SDL_Rect p;
-} enigme;
-
-int initialiser_enigme();
-// void generer(SDL_Surface * screen  , char image [],enigme *E ,int *aleatoire);
+int initialiser_enigmes();
+void generation(enigme *E,int val);
 void affichage(enigme *E ,int val);
-int resolution_enigmes(char reponse_choisie[3], int val,enigme *E);
-int solution_enigme (char image [], int *reponse) ;
-int resolution (int * running ,int *run);
-int afficher_resultat (SDL_Surface * screen,int s,int r,enigme *EN, int reponse) ;
+int resolution_enigmes(char reponse_choisie[4], int val,enigme *E);
 
 #endif  /* ENIGME_H_ */
